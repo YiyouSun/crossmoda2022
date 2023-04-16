299b851edddba270c099cc40bcceb1dec448cf7e
Yiyou Sun <67134461+YiyouSun@users.noreply.github.com>
2023-04-16 11:23:10 +0200

    Update test3d.sh

--- unet/test3d.sh
+++ unet/test3d.sh
@@ -1,1 +1,1 @@
-python -u test_3D.py --root_path ../../../nnunet/DA/data --num_classes 3 --exp MoDA/Mean_Teacher_50 --model unet_3D
+python -u test_3D.py --root_path ../data/unet --num_classes 3 --exp MoDA/Mean_Teacher_189 --model unet_3D
